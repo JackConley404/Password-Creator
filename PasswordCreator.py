fda637310fbd1c7bc6c87facc3d8c67d7fef0da9
Jack Conley <53314816+JackConley404@users.noreply.github.com>
2021-04-20 18:53:48 -0400

    Update PasswordCreator.py

--- PasswordCreator.py
+++ PasswordCreator.py
@@ -1,4 +1,3 @@
-
 import secrets
 import string
 
@@ -6,10 +5,34 @@ letters = string.ascii_letters
 numbers = string.digits
 punc = string.punctuation
 
-usableChars = letters + numbers + punc
+usableChars = letters
+
+allowedInput = ['Y', 'y', 'N', 'n']
+
+
+useNums = input("Use numbers? (Y/N): ")
+while useNums not in allowedInput:
+    print(useNums, "is an invalid input.")
+    useNums = input("Use numbers? (Y/N): ")
+     
+if useNums == 'Y' or useNums == 'y':
+    usableChars += numbers
+
+
+
+usePunc = input("Use punctuation? (Y/N): ")
+while usePunc not in allowedInput:
+    print(usePunc, "is an invalid input.")
+    usePunc = input("Use punctuation? (Y/N): ")
+     
+if useNums == 'Y' or useNums == 'y':
+   usableChars += punc
+
 
 
 passwordLength = int(input("Enter password length: "))
+
+
 
 def passGen(passwordLength, usableChars):
     return ''.join(secrets.choice(usableChars) for i in range(passwordLength))
