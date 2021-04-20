13bc1d90c8d441e57ae9211d6876253602273400
Jack Conley <53314816+JackConley404@users.noreply.github.com>
2021-04-20 18:30:59 -0400

    Update PasswordCreator.py

--- PasswordCreator.py
+++ PasswordCreator.py
@@ -1,1 +1,17 @@
 
+import secrets
+import string
+
+letters = string.ascii_letters
+numbers = string.digits
+punc = string.punctuation
+
+usableChars = letters + numbers + punc
+
+
+passwordLength = int(input("Enter password length: "))
+
+def passGen(passwordLength, usableChars):
+    return ''.join(secrets.choice(usableChars) for i in range(passwordLength))
+
+print("Generated password:", passGen(passwordLength, usableChars))
